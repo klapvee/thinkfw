6df398966c3c63879342c8408fa8e1c1ef506515
Willem Daems <willem@Willems-MacBook-Pro.local>
2012-07-10 10:17:04 +0200

    Added seperate rendering in View.php, added sample code in index controller

--- application/Controllers/IndexController.php
+++ application/Controllers/IndexController.php
@@ -1,23 +1,34 @@
 <?php
 
-	class IndexController extends Base_Controller_Action
-	{
-		private $_model;
-		private $_db;
-		
-		public function init()
-		{
-			if (!isset($_SESSION['user']))
-			{
-				Base_Application::redirect("/login");
-			}
-			
-			$this->_model = new Model_Index;
-			$this->_db = Base_Application::getDatabase();
-		}
-		
-		public function indexAction()
-		{
-			$this->view->categories = $this->_model->getCategories($this->_db);
-		}
-	}
+    class IndexController extends Base_Controller_Action
+    {
+        private $_model;
+        private $_db;
+
+        public function init()
+        {
+            if (!isset($_SESSION['user']))
+            {
+                    Base_Application::redirect("/login");
+            }
+
+            $this->_model = new Model_Index;
+            $this->_db = Base_Application::getDatabase();
+        }
+
+        public function indexAction()
+        {
+            /*
+             * sample code rendering view
+             * 
+             * $divider = new Base_View;
+             * $divider->setView('index/divider.phtml');
+             * $divider->pages = $count / $this->_limit;
+             * $divider->active = $this->_page;
+             * $divider->limit = $this->_limit;
+             * 
+             * $content = $divider->render();
+             *
+             */
+        }
+    }
--- library/Base/View.php
+++ library/Base/View.php
@@ -1,88 +1,189 @@
 <?php
 
-	class Base_View
-	{	
-		private $layout;
-		private $view;
-		private $render;
-		public $content;
-		public $path;
-		public $frontController;
-		
-		
-		public function __construct()
-		{
-			$this->render = true;
-			$this->layout = 'layout.phtml';
-		}
-		
-		public function getView()
-		{
-			
-		}
+    class Base_View
+    {	
+        
+        /**
+         *
+         * @var string 
+         */
+        private $layout;
+        
+        /**
+         *
+         * @var string 
+         */
+        private $view;
+        
+        /**
+         *
+         * @var string 
+         */
+        private $renderView;
+        
+        /**
+         *
+         * @var string 
+         */
+        public $content;
+        
+        /**
+         *
+         * @var string 
+         */
+        public $path;
+        
+        /**
+         *
+         * @var string 
+         */
+        public $frontController;
+        
+        /**
+         *
+         * @var string 
+         */
+        public $pageTitle;
+        
+        /**
+         *
+         * @var string 
+         */
+        public $contentBottom;
+        
+        /**
+         *
+         * @var string 
+         */
+        public $contentSidebar;
 
-		public function setFrontController($controller)
-		{
-			$this->frontController = $controller;
-		}
+        
+        public function __construct()
+        {
+            $this->renderView = true;
+            $this->layout = 'layout.phtml';
+        }
 
-		public function getFrontController()
-		{
-			return $this->frontController;
-		}
-		
-		public function getLayout()
-		{
-			if (!$this->render) return;
-			require APPLICATION_PATH . '/layouts/' . $this->layout;
-		}
-		
-		public function content()
-		{
-			if (!$this->render) return;
-			$dir = empty( $this->path[1]) ? 'index' : $this->path[1];
-						
-			if (empty($this->view))
-			{
-				$file = empty( $this->path[2]) ? 'index' : $this->path[2];
-			} else {
-				$file = $this->view;
-			}
+        public function getView()
+        {
 
-			if (is_file(APPLICATION_PATH . '/views/html/'.$dir . '/' . $file . '.phtml'))
-			{
-				require APPLICATION_PATH . '/views/html/'.$dir . '/' . $file . '.phtml'; 
-			}
-		}
-		
-		public function setView($view)
-		{
-			$this->view = $view;
-		}
-		
-		public function setLayout($layout)
-		{
-			$this->layout = $layout;
-		}
-		
-		public function setRender($render)
-		{
-			$this->render = $render;
-		}
-		
-		public function show()
-		{
-			
-			$this->getLayout();
-		}
-		
-		public function __isset($name)
-		{
-			
-		}
-		
-		public function __set($key, $value)
-		{
-			$this->$key = $value;
-		}
-	}
+        }
+        
+        /**
+         *
+         * @param type $content 
+         */
+        public function setSidebar($content)
+        {
+            $this->contentSidebar .= $content;
+        }
+        
+        /**
+         *
+         * @param type $content 
+         */
+        public function setBottom($content)
+        {
+            $this->contentBottom .= $content;
+        }
+
+        public function setPageTitle($title)
+        {
+            $this->pageTitle = $title;
+        }
+
+        public function setFrontController($controller)
+        {
+            $this->frontController = $controller;
+        }
+
+        public function getFrontController()
+        {
+            return $this->frontController;
+        }
+
+        public function getLayout()
+        {
+            if (!$this->renderView) return;
+            require APPLICATION_PATH . '/layouts/' . $this->layout;
+        }
+
+        public function pageTitle()
+        {
+            return $this->pageTitle;
+        }
+
+        public function content()
+        {
+            if (!$this->renderView) return;
+            $dir = empty( $this->path[1]) ? 'index' : $this->path[1];
+
+            if (empty($this->view))
+            {
+                $file = empty( $this->path[2]) ? 'index' : $this->path[2];
+            } else {
+                $file = $this->view;
+            }
+
+            if (is_file(APPLICATION_PATH . '/views/html/'.$dir . '/' . $file . '.phtml'))
+            {
+                require APPLICATION_PATH . '/views/html/'.$dir . '/' . $file . '.phtml'; 
+            }
+        }
+
+        public function bottom()
+        {
+            return $this->contentBottom;
+        }
+
+        public function sidebar()
+        {
+            return $this->contentSidebar;
+        }
+
+        public function render()
+        {	
+            if (file_exists(APPLICATION_PATH . '/views/html/' . $this->view)) {
+
+                ob_start();
+
+                require APPLICATION_PATH . '/views/html/' . $this->view;
+                $content = ob_get_contents();
+                ob_end_clean();
+                return $content;
+            } else {
+                echo 'view not found';
+            }
+        }
+
+        public function setView($view)
+        {
+            $this->view = $view;
+        }
+
+        public function setLayout($layout)
+        {
+            $this->layout = $layout;
+        }
+
+        public function setRender($render)
+        {
+            $this->renderView = $render;
+        }
+
+        public function show()
+        {
+
+            $this->getLayout();
+        }
+
+        public function __isset($name)
+        {
+
+        }
+
+        public function __set($key, $value)
+        {
+            $this->$key = $value;
+        }
+    }
